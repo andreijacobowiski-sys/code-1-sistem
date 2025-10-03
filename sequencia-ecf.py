dec90f4aa93b1547958b17f95c12d4f0ed4d7932
Andrei jacobowiski 1 sistemas <andrei.jacobowiski@escola.pr.gov.br>
2025-10-03 07:43:29 -0300

    Add files via upload

---
+++ sequencia-ecf.py
@@ -0,0 +1,8 @@
+print("*** Mini Questionário")
+nome = input("qual seu nome?")
+idade = input("qual a sua idade?")
+genero = input("voce é homem ou mulher?")
+endereco = input("qual o seu endereço?\n")
+
+print("olá,", nome, "!")
+print("")
