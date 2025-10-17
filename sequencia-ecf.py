1d5a4d91e802bb5d2b9a0bdbc3201745d3438c30
Andrei jacobowiski 1 sistemas <andrei.jacobowiski@escola.pr.gov.br>
2025-10-17 07:57:28 -0300

    Update sequencia-ecf.py

--- sequencia-ecf.py
+++ sequencia-ecf.py
@@ -1,16 +1,27 @@
 Import os
 Import sys
 
-print("*** Mini Questionário\n")
-nome = input("Qual seu nome?\n")
-idade = input("Qual a sua idade?\n")
-genero = input("Voce é homem ou mulher?\n")
-endereco = input("Qual o seu endereço?\n")
 
-os.system('cls')
+def leitura_dados():
+    print("*** Mini Questionário\n")
+    nome = input("Qual seu nome?\n")
+    idade = input("Qual a sua idade?\n")
+    genero = input("Voce é homem ou mulher?\n")
+    endereco = input("Qual o seu endereço?\n")
 
-print("*** Mini Questionário\n")
-print("olá,", nome, "\n")
-print("Idade: ", Idade, "\n")
-print("Gênero: ", Gênero, "\n")
-print("Endreço: ", Endereco, "\n")
+    return nome, idade, genero, endereco
+
+def impressao_dados(nome, idade, genero, endereco):
+    print("*** Mini Questionário\n")
+    print("olá,", nome, "\n")
+    print("Idade: ", Idade, "\n")
+    print("Gênero: ", Gênero, "\n")
+    print("Endreço: ", Endereco, "\n")
+
+def iniciar_app():
+    leitura_dados()
+    os.system('cls')
+    impressao_dados()
+
+iniciar_app()
+
